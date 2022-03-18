d50dedb1c0388ea1a4c88e9d7d56e43ea54ca8d8
@s_zhsn <s.zhosan@gmail.com>
2022-03-18 12:08:02 +0200

    h-w-react-1-precess

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
+import Profile from './Profile/Profile';
+import Container from './Container/Container';
+import user from './user';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </Container>
   );
 };
